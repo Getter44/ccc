35c63449a993ca91f17f955db7abe96f6548d151
12227642 <e12227642@student.tuwien.ac.at>
2023-10-20 16:30:13 +0200

    first draft

--- src/Main.java
+++ src/Main.java
@@ -40,18 +40,42 @@ public class Main {
         int x1, x2, y1, y2;
         for (int i = 0; i < o; i++) {
             s = reader.nextLine();
+
+
             String s1 = s.substring(0, s.indexOf(' '));
-            String s2 = s.substring(s.indexOf(' ')+1);
+            String s2 = s.substring(s.indexOf(' ') + 1);
             x1 = Integer.parseInt(s1.substring(0, s1.indexOf(',')));
-            y1 = Integer.parseInt(s1.substring(s1.indexOf(',')+1));
+            y1 = Integer.parseInt(s1.substring(s1.indexOf(',') + 1));
 
             x2 = Integer.parseInt(s2.substring(0, s2.indexOf(',')));
-            y2 = Integer.parseInt(s2.substring(s2.indexOf(',')+1));
+            y2 = Integer.parseInt(s2.substring(s2.indexOf(',') + 1));
 
-            System.out.println(x1 + " " + y1);
+            //System.out.println(x1 + " " + y1);
+
+            if (rec(x1, y1, x2, y2, Arr)) {
+                writer.write("SAME");
+            } else {
+                writer.write("DIFFERENT");
 
 //            writer.write(Arr[Integer.parseInt(s.substring(s.indexOf(',')+1))][Integer.parseInt(s.substring(0, s.indexOf(',')))] + "\n");
             }
+        }
+    }
+
+
+        public static boolean rec(int x1, int y1, int x2, int y2, char[][] field){
+            if (x1 == x2 && y1 == y2) {
+                return true;
+            } else if (field[x1][y1] == 'W') {
+                return false;
+            }
+
+            return rec(x1 + 1, y1, x2, y2, field) ||
+                    rec(x1 - 1, y1, x2, y2, field) ||
+                    rec(x1, y1 + 1, x2, y2, field) ||
+                    rec(x1, y1 - 1, x2, y2, field);
+
+
         }
 
         //write result to file
